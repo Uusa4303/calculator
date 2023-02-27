83b5503d588324bfc7d939dc94cdb4fe802f0e27
your_username <varshithkonakanchi>
2023-02-27 11:57:22 +0530

    addition file

---
+++ varshith_addition.py
@@ -0,0 +1,8 @@
+while True:
+    
+    num1=int(input('enter  first number\n'))
+    num2=int(input('enter second number\n'))
+    print("1.addition 2.subtraction 3.multiplication 4.division")
+    opt =int(input("enter the operation (number) you want to do \n"))
+    if opt ==1:
+        print(num1+num2)
