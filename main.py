9e55d66794ba8b555c36491688091aa4e4b7f77d
Balaji Avinash <balajiavinash7461@gmail.com>
2023-02-28 16:46:21 +0530

    applied patch

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 while True:
     
-    num1=int(input('enter  first number\n'))
-    num2=int(input('enter second number\n'))
+    num1=float(input('enter  first number\n'))
+    num2=float(input('enter second number\n'))
     print("1.addition 2.subtraction 3.multiplication 4.division")
-    opt =int(input("enter the operation (number) you want to do \n"))
+    opt =float(input("enter the operation (number) you want to do \n"))
     if opt ==1:
         print(num1+num2)
     elif opt==2:
@@ -11,7 +11,7 @@ while True:
     elif opt==3:
         print(num1*num2)
     elif opt==4:
-        print(num1//num2)
+        print(num1/num2)
         
     
     else:
