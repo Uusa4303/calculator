05f46f1cc7e78ebbc7708df8e097b52b8acb2a3b
harshaljain-dev <harshal.jain20@st.niituniversity.in>
2023-03-01 00:06:15 -0500

    Fixed the flaw which messed up our Presentation.

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-while True:
-    
-    num1=float(input('enter  first number\n'))
-    num2=float(input('enter second number\n'))
-    print("1.addition 2.subtraction 3.multiplication 4.division")
-    opt =float(input("enter the operation (number) you want to do \n"))
-    if opt ==1:
-        print(num1+num2)
-    elif opt==2:
-        print(num1-num2)
-    elif opt==3:
-        print(num1*num2)
-    elif opt==4:
-        print(num1/num2)
-        
-    
-    else:
-        print("please check the input")
+# define the function to perform calculations
+def calculate(numbers, operators):
+    result = numbers[0]
+    for i in range(1, len(numbers)):
+        if operators[i-1] == '+':
+            result += numbers[i]
+        elif operators[i-1] == '-':
+            result -= numbers[i]
+        elif operators[i-1] == '*':
+            result *= numbers[i]
+        elif operators[i-1] == '/':
+            if numbers[i] == 0:
+                result = 'infinity'
+            else:
+                result /= numbers[i]
+        else:
+            result = 'invalid operator'
+            break
+    return result
 
-    break
+# get user input
+numbers = []
+operators = []
+while True:
+    try:
+        num = float(input("Enter a number: "))
+    except ValueError:
+        break
+    numbers.append(num)
+    if len(numbers) > 1:
+        op = input("Enter an operator (+, -, *, /) or '=' to calculate: ")
+        if op == '=':
+            break
+        operators.append(op)
+        
+# perform the calculation and display the result
+result = calculate(numbers, operators)
+print("Result: ", result)
